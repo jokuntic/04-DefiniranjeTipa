40711574342d8b22a8f07ae76b287bf982e8bae2
Josip Kuntic <jokuntic@vsite.hr>
2016-11-03 19:37:48 +0100

    Završen destruktor.

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -15,8 +15,13 @@ namespace Vsite.CSharp
             Debug.WriteLine(poruka);
         }
 
-        // TODO: Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
-
+        //  Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
+        ~KlasaSDestruktorom()
+        {
+            Console.WriteLine($"Destruktor objekta br. {RedniBroj}.");
+            Debug.WriteLine($"Destruktor objekta br. { RedniBroj}.");
+        }
+            
 
         public readonly int RedniBroj; // redni broj objekta
 
@@ -33,7 +38,10 @@ namespace Vsite.CSharp
             }
 
             // TODO: Dodati for petlju koja će se ponoviti 100000 puta i unutar koje će se kreirati novi objekti tipa KlasaSDestruktorom. Pokrenuti program i provjeriti redni broj prvog uništenog objekta.
-
+            for (int i = 0; i < 100000; ++i)
+            {
+                new KlasaSDestruktorom();
+            }
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
 
